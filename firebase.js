42c9b2c69eae72ea53991b9100ffead7cd4030c7
Ayoe <anomalous189@gmail.com>
2022-06-06 07:46:43 +0100

    first commit

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCXvXREbhBPiMsrfwQIf6ileyvePgsgRa8",
+    authDomain: "disney-clone-dfbe0.firebaseapp.com",
+    projectId: "disney-clone-dfbe0",
+    storageBucket: "disney-clone-dfbe0.appspot.com",
+    messagingSenderId: "249211552525",
+    appId: "1:249211552525:web:01f78a7c12100eec48a644",
+    measurementId: "G-9Z57JLLN0J"
+};
+npm 
+PopStateEvent.length ? firebase.initializeApp(firebaseConfig) : firebase.app(y)
+
+const app = !firebase.a
